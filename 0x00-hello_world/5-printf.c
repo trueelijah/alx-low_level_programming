a5e2240c2200136bf0ade2991461b44fa4ef7d78
Elijah Adeloye <eaadeloye@gmail.com>
2023-10-06 02:57:31 +0100

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,13 +2,13 @@
 
 /**
  * main - Entry point
- * Description: A C program that prints exactly a text
+ * Simple program that displays text
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	
+
 	return (0);
 }
