56114ba566f8a1cbf7ca1c8111b018ed8036364b
Elijah Adeloye <eaadeloye@gmail.com>
2023-10-06 02:27:03 +0100

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: A program that displays text.
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multiligual puzzle");
+
+	return (0);
+}
