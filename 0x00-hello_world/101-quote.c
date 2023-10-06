33a56e47566d0be8110a07b8beb4756148cdf219
Elijah Adeloye <eaadeloye@gmail.com>
2023-10-06 03:39:00 +0100

    Unix is basically a simple operating system

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
-#include <unistd.h>
+#include<unistd.h>
 
 /**
  * main - Entry point of the program
@@ -12,14 +12,17 @@
 
 int main(void)
 {
-	char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	int i = 0;
-	while (message[i] != '\0')
+    char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+    int i = 0;
+
+    while (message[i] != '\0')
     {
 	    write(2, &message[i], 1);
 	    i++;
     }
     return (1);
-    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-    return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
+	return (1);
 }
