418be5c30a20f58c369e0a6e085a548857f2e3c2
Elijah Adeloye <eaadeloye@gmail.com>
2023-10-07 19:42:56 +0100

    Patience, persistence and perspiration

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * A program that prints all the number in base 16
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char digit;
+
+	for (digit = '0' ; digit <= '9' ; digit++)
+	{
+		putchar(digit);
+
+		if (digit < '9')
+		{
+		putchar(',');
+		putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
