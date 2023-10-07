7196e97687c6e80c47d6f18bf4b6f3ede0efeb74
Elijah Adeloye <eaadeloye@gmail.com>
2023-10-07 18:17:09 +0100

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,17 +2,22 @@
 
 /**
  * main - Entry point
- * A program that prints alphabet in lovwer case
+ * A program that prints alphabet in lower case
  * Return: Always 0 (success)
  */
 
 int main(void)
 {
 	char letter;
+	char LETTER;
 
 	for (letter = 'a' ; letter <= 'z' ; letter++)
 	{
 		putchar(letter);
+	}
+	for (LETTER = 'A' ; LETTER <= 'Z' ; LETTER++)
+	{
+		putchar(LETTER);
 	}
 	putchar('\n');
 
