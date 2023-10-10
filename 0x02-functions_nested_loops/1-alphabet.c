ce0e601ea357117b7f2a0935ac449ba25045e659
Elijah Adeloye <eaadeloye@gmail.com>
2023-10-10 16:51:19 +0100

    Add a C program to print the lowercase alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - 
+ * A program that prints alphabet
+ * Return: always 0 (success)
+ */
+int main(void)
+
+{
+	char letter
+		for (letter = 'a', letter <= 'z', letter++);
+	{
+	putchar(letter);
+	}
+	putchar(\n);
+
+	return (0);
+}
