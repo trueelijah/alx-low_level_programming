571be0b2df153543e5001b479f8f385fa18ead97
Elijah Adeloye <eaadeloye@gmail.com>
2023-10-12 00:26:09 +0100

    A function to print every minute of the day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -15,7 +15,7 @@ void jack_bauer(void)
 		{
 			_putchar((hours / 10) + '0');
 			_putchar((hours % 10) + '0');
-			_putchar(';');
+			_putchar(':');
 			_putchar((minutes / 10) + '0');
 			_putchar((minutes % 10) + '0');
 			_putchar('\n');
