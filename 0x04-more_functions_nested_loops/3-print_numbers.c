ce64f2766e5db307ef0ea80a8d62569885fa200c
Elijah Adeloye <eaadeloye@gmail.com>
2023-10-12 23:19:05 +0100

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - a function that prints numbers
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
