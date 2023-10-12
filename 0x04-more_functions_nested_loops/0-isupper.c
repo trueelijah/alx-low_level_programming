9228fd1e6cb07a6c3bdbfa00e689780f1f7ea7b4
Elijah Adeloye <eaadeloye@gmail.com>
2023-10-12 19:17:13 +0100

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: character
+ * Return: 0
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z'; c++)
+	{
+		return (1);
+	}
+	else
+		return (0);
+
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Readme for 0x04.C - More functions, more nested loops
