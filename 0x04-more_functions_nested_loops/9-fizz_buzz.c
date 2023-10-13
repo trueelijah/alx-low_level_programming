33de46c77bb5b9bd26ea27e921e927494b43f4bc
Elijah Adeloye <eaadeloye@gmail.com>
2023-10-13 05:53:10 +0100

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+*/
+
+int main(void)
+{
+	int i;
+
+	char f[] = "Fizz";
+
+	char b[] = "Buzz";
+
+	char fb[] = "FizzBuzz";
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i == 100)
+		{
+			printf("%s", b);
+		}
+		else if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			printf("%s ", fb);
+		}
+		else if (i % 3 == 0)
+		{
+			printf("%s ", f);
+		}
+		else if (i % 5 == 0)
+		{
+			printf("%s ", b);
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+
+	}
+	printf("\n");
+	return (0);
+}
