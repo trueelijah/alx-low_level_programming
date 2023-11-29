b2759d1929d099e76499bf2b883e4b88874b8911
Elijah Adeloye <eaadeloye@gmail.com>
2023-11-29 17:01:22 +0100

    a program that copies the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -5,7 +5,7 @@ void close_file(int fd);
 
 /**
  * create_buffer - Allocates 1024 bytes for a buffer.
- * @file: The name of the file buffer is storing chars for.
+ * @file: The name of the file.
  *
  * Return: A pointer to the newly-allocated buffer.
  */
@@ -43,16 +43,16 @@ void close_file(int fd)
 }
 
 /**
- * main - Copies the contents of a file to another file.
- * @argc: The number of arguments supplied to the program.
+ * main - Copies one file to another file.
+ * @argc: The number of arguments.
  * @argv: An array of pointers to the arguments.
  *
  * Return: 0 on success.
  *
- * Description: If the argument count is incorrect - exit code 97.
- *              If file_from does not exist or cannot be read - exit code 98.
- *              If file_to cannot be created or written to - exit code 99.
- *              If file_to or file_from cannot be closed - exit code 100.
+ * Description: If the argument count is incorrect - exit 97.
+ * If file_from does not exist or cannot be read - exit 98.
+ * If file_to cannot be created or written to - exit 99.
+ * If file_to or file_from cannot be closed - exit 100.
  */
 int main(int argc, char *argv[])
 {
